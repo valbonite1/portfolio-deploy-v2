c2ecea0e1733e8882a459cae455607590db5d4a2
valbonite1 <valbonite@gmail.com>
2020-11-03 00:54:38 +0800

    Updates

---
+++ precache-manifest.41ef5b1a3b3499c7dac21d22f9992bcd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2a3b13d5ceb0e5e9343e90664d2426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4eeead6c8b141d3d058",
+    "url": "/static/css/main.4f300e35.chunk.css"
+  },
+  {
+    "revision": "d8ae9db2be40129cd5d4",
+    "url": "/static/js/2.2bc33898.chunk.js"
+  },
+  {
+    "revision": "829006946e4ad2c5d75a5ad802dea8b1",
+    "url": "/static/js/2.2bc33898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4eeead6c8b141d3d058",
+    "url": "/static/js/main.6b682227.chunk.js"
+  },
+  {
+    "revision": "863da4673731285c8dea",
+    "url": "/static/js/runtime-main.6971d9f0.js"
+  },
+  {
+    "revision": "ec1d212afa32b2f81b5721858291f179",
+    "url": "/static/media/BONITE.ec1d212a.png"
+  },
+  {
+    "revision": "f225c632b778d0ac5c48f8032d6f2d1f",
+    "url": "/static/media/campaign_launch.f225c632.png"
+  },
+  {
+    "revision": "3b88f9d353fc13d68da4130b522bd506",
+    "url": "/static/media/loading-projects.3b88f9d3.png"
+  },
+  {
+    "revision": "2062d77da8946494fbd754dca0541745",
+    "url": "/static/media/white-logo.2062d77d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-deploy-v1/precache-manifest.2a1ce00dd3f02462a59451a9eaa14495.js"
+  "/precache-manifest.41ef5b1a3b3499c7dac21d22f9992bcd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-deploy-v1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
